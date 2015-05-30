3bcba6d6cbd01e106925cae6b977a78ed324466e
Chuck Mcandrew <chuck.mcandrew@leblibrary.com>
2015-05-30 16:18:40 -0400

    add @api-view to views

--- ticket_app/views.py
+++ ticket_app/views.py
@@ -1,33 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from ticket_app.models import Transactions, Tickets, Locations, PaymentTypes
 from ticket_app.serializers import TicketSerializer, TransactionSerializer
 
 # Create your views here.
-class JSONResponse(HttpResponse):
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
-
-@csrf_exempt
-def ticket_list(request):
+@api_view(['GET', 'POST'])
+def ticket_list(request, format=None):
     """
-    List all landfill tickets or add landfill tickets
+    List all tickets or create a new ticket
     :param request:
     :return:
     """
     if request.method == 'GET':
         tickets = Tickets.objects.all()
         serializer = TicketSerializer(tickets, many=True)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
+
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = TicketSerializer(data=data)
+        serializer = TicketSerializer(data = request.data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data, status=201)
-        return JSONResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
