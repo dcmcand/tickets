888ab58079644a6f8fdd97740f8bb7a548adce24
Chuck Mcandrew <chuck.mcandrew@leblibrary.com>
2015-05-30 15:06:53 -0400

    changed to ModelSerializer from Serializer in serializers.py

--- ticket_app/views.py
+++ ticket_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from ticket_app.models import Transactions, Tickets, Locations, PaymentTypes
+from
 
 # Create your views here.
+
+
