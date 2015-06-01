7f6206202ca0f532b3642fb3e3e97cb1bd6de203
Chuck Mcandrew <chuck.mcandrew@leblibrary.com>
2015-06-01 19:17:12 -0400

    Started to add templates

--- ticket_app/urls.py
+++ ticket_app/urls.py
@@ -3,11 +3,11 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = {
-    url(r'^tickets/audit/?$', views.TicketAudit.as_view()),
-    url(r'^tickets/?$', views.TicketList.as_view()),
-    url(r'^transactions/report/?$', views.TransactionReport.as_view()),
-    url(r'^transactions/(?P<pk>[0-9]+)/?$', views.TransactionDetail.as_view()),
-    url(r'^transactions/?$', views.TransactionList.as_view()),
+    url(r'^api/tickets/audit/?$', views.TicketAudit.as_view()),
+    url(r'^api/tickets/?$', views.TicketList.as_view()),
+    url(r'^api/transactions/report/?$', views.TransactionReport.as_view()),
+    url(r'^api/transactions/(?P<pk>[0-9]+)/?$', views.TransactionDetail.as_view()),
+    url(r'^api/transactions/?$', views.TransactionList.as_view()),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- tickets/urls.py
+++ tickets/urls.py
@@ -20,5 +20,4 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 	url(r'^', include('ticket_app.urls')),
     url(r'^api-auth', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider'))
 ]
