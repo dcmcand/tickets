38f345a47733c782f9500371f199cf98216f2d1c
Chuck McAndrew <chuck.mcandrew@leblibrary.com>
2015-06-02 15:15:35 -0400

    started adding forms

--- ticket_app/views.py
+++ ticket_app/views.py
@@ -2,11 +2,17 @@
 from rest_framework import generics
 from ticket_app.models import Transactions, Tickets
 from ticket_app.serializers import TicketSerializer, TransactionSerializer
+from django.utils.decorators import method_decorator
+from django.views.generic import View
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from .forms import TransactionForm
 
 class TicketList(generics.ListCreateAPIView):
     """
     List all Tickets or Create a new Ticket
     """
+    @method_decorator(login_required)
     queryset = Tickets.objects.all()
     serializer_class = TicketSerializer
 
@@ -14,6 +20,7 @@ class TicketAudit(generics.ListAPIView):
     """
     Shows unsold tickets
     """
+    @method_decorator(login_required)
     queryset = Tickets.objects.exclude(sold=True)
     serializer_class = TicketSerializer
 
@@ -22,6 +29,7 @@ class TransactionList(generics.ListCreateAPIView):
     """
     List all Transactions or Create a new Transaction
     """
+    @method_decorator(login_required)
     queryset = Transactions.objects.all()
     serializer_class = TransactionSerializer
 
@@ -29,6 +37,7 @@ class TransactionDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update, or delete a transaction instance
     """
+    @method_decorator(login_required)
     queryset = Transactions.objects.all()
     serializer_class = TransactionSerializer
 
@@ -36,7 +45,11 @@ class TransactionReport(generics.ListAPIView):
     """
     Shows unreported transactions
     """
+    @method_decorator(login_required)
     queryset = Transactions.objects.exclude(reported=True)
     serializer_class = TransactionSerializer
 
-
+class TransactionForm(View):
+    def get(self, request):
+        return render(request, TransactionForm)
+    
