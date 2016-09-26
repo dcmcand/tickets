11191b93f59dac3901a6a2218432230e789c5cb1
Chuck McAndrew <chuck.mcandrew@leblibrary.com>
2016-09-26 13:07:02 -0400

    Fixed bug that wouldn't display transaction reports.

--- ticket_app/urls.py
+++ ticket_app/urls.py
@@ -2,12 +2,12 @@ from django.conf.urls import url, include
 from . import views
 
 
-api_urls = [
-    url(r'^transactions/report/?$', views.ApiTransactionReport.as_view()),
-    url(r'^transactions/(?P<pk>[0-9]+)/?$', views.ApiTransactionDetail.as_view()),
-    url(r'^transactions/?$', views.ApiTransactionList.as_view()),
-    url(r'^tickets/?$', views.ApiTicketList.as_view()),
-]
+#api_urls = [
+#    url(r'^transactions/report/?$', views.ApiTransactionReport.as_view()),
+#    url(r'^transactions/(?P<pk>[0-9]+)/?$', views.ApiTransactionDetail.as_view()),
+#    url(r'^transactions/?$', views.ApiTransactionList.as_view()),
+#    url(r'^tickets/?$', views.ApiTicketList.as_view()),
+#]
 
 
 tickets_urls = [
@@ -20,17 +20,18 @@ tickets_urls = [
 transactions_urls = [
     #url(r'^/?$', views.TransactionsList.as_view(), name="Transactions List"),
     url(r'^$', views.TransactionsList.as_view(), name="Transactions List"),
-    # url(r'^detail/?$', views.TransactionDetail, name="Transaction Detail"),
+     url(r'^detail/?$', views.TransactionDetail, name="Transaction Detail"),
     url(r'^detail/(?P<pk>\d+)/?$', views.TransactionDetail.as_view(), name="Transaction Detail"),
 
 ]
 
 
 urlpatterns = [
-    url(r'^report/(?P<pk>[0-9])/?$', views.ReportArchive.as_view(), name='Report Archive'),
+    url(r'^report/(?P<pk>\d+)/?$', views.ReportArchive.as_view(), name='Report Archive'),
+    #url(r'^report/(?P<pk>[0-9])/?$', views.ReportArchive.as_view(), name='Report Archive'),
     url(r'^report/?$', views.GenerateReport.as_view()),
     url(r'^tickets/', include(tickets_urls)),
-    url(r'^api/', include(api_urls)),
+    #url(r'^api/', include(api_urls)),
     url(r'^transactions/', include(transactions_urls)),
     url(r'^$', views.AddTransaction.as_view()),
     #url(r'^/?$', views.AddTransaction.as_view()),
